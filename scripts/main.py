fe6694a292561fb6e9d0b2736af94df13fb9939b
FULIN2024 <FULIN.LI@BARUCHMAIL.CUNY.EDU>
2024-04-19 13:30:56 -0400

    adding datd dictionary

--- scripts/main.py
+++ scripts/main.py
@@ -7,4 +7,6 @@ config_file_path ='config/config.json'
 with open(config_file_path, 'r') as config_file:
     config = json. load(config_file)
 #Print the configuration 
-Connection_STRING = fconfig(["connectionString"])
+Connection_STRING = config(["connectionString"])
+
+print(connectionString)
