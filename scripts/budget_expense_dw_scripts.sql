d27a6426b978d06cdbd7cfc9cd953f8ef7aa62f3
FULIN2024 <FULIN.LI@BARUCHMAIL.CUNY.EDU>
2024-05-09 00:15:49 -0400

    Change some sql

--- scripts/budget_expense_dw_scripts.sql
+++ scripts/budget_expense_dw_scripts.sql
@@ -1,48 +1,48 @@
-CREATE SCHEMA IF NOT EXISTS "theBudget_Expense";
+CREATE SCHEMA IF NOT EXISTS "TheBudget_Expense";
 
-CREATE  TABLE "theBudget_Expense".dim_activity ( 
+CREATE  TABLE "TheBudget_Expense".dim_activity ( 
 	activitycode         VARCHAR  NOT NULL  ,
 	activityname         VARCHAR(100)    ,
 	CONSTRAINT pk_dim_activity PRIMARY KEY ( activitycode )
  );
 
-CREATE  TABLE "theBudget_Expense".dim_department ( 
+CREATE  TABLE "TheBudget_Expense".dim_department ( 
 	departmentcode       INT  NOT NULL  ,
 	departmentname       VARCHAR(100)    ,
 	CONSTRAINT pk_dim_department PRIMARY KEY ( departmentcode )
  );
 
-CREATE  TABLE "theBudget_Expense".dim_dept_rollup ( 
+CREATE  TABLE "TheBudget_Expense".dim_dept_rollup ( 
 	dept_rollup          VARCHAR  NOT NULL  ,
 	dept_rollup_name     VARCHAR(100)    ,
 	CONSTRAINT pk_dim_dept_rollup PRIMARY KEY ( dept_rollup )
  );
 
-CREATE  TABLE "theBudget_Expense".dim_expense ( 
+CREATE  TABLE "TheBudget_Expense".dim_expense ( 
 	expensecode          VARCHAR  NOT NULL  ,
 	expensename          VARCHAR(100)    ,
 	CONSTRAINT pk_dim_expensecategory PRIMARY KEY ( expensecode )
  );
 
-CREATE  TABLE "theBudget_Expense".dim_fund ( 
+CREATE  TABLE "TheBudget_Expense".dim_fund ( 
 	fundcode             VARCHAR  NOT NULL  ,
 	fundname             VARCHAR(100)    ,
 	CONSTRAINT pk_fundcode PRIMARY KEY ( fundcode )
  );
 
-CREATE  TABLE "theBudget_Expense".dim_program ( 
+CREATE  TABLE "TheBudget_Expense".dim_program ( 
 	programcode          VARCHAR  NOT NULL  ,
 	programname          VARCHAR(100)    ,
 	CONSTRAINT pk_dim_program PRIMARY KEY ( programcode )
  );
 
-CREATE  TABLE "theBudget_Expense".dim_unit ( 
+CREATE  TABLE "TheBudget_Expense".dim_unit ( 
 	unitcode             VARCHAR  NOT NULL  ,
 	unitname             VARCHAR(100)    ,
 	CONSTRAINT pk_dim_unit PRIMARY KEY ( unitcode )
  );
 
-CREATE  TABLE "theBudget_Expense"."facts_budget/expense" ( 
+CREATE  TABLE "TheBudget_Expense"."facts_budget/expense" ( 
 	fact_id              BIGINT  NOT NULL  ,
 	budget_fiscal_year   INT    ,
 	thru_quarter         INT    ,
