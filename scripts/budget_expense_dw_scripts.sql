37e440209b568c229ad0276ae7f964af06ddcb96
FULIN2024 <FULIN.LI@BARUCHMAIL.CUNY.EDU>
2024-05-08 23:41:23 -0400

    make some change on sql

--- scripts/budget_expense_dw_scripts.sql
+++ scripts/budget_expense_dw_scripts.sql
@@ -1,48 +1,48 @@
-CREATE SCHEMA IF NOT EXISTS "BudgetExpense";
+CREATE SCHEMA IF NOT EXISTS "Budget_Expense";
 
-CREATE  TABLE "BudgetExpense".dim_activity ( 
+CREATE  TABLE "Budget_Expense".dim_activity ( 
 	activitycode         VARCHAR  NOT NULL  ,
 	activityname         VARCHAR(100)    ,
 	CONSTRAINT pk_dim_activity PRIMARY KEY ( activitycode )
  );
 
-CREATE  TABLE "BudgetExpense".dim_department ( 
+CREATE  TABLE "Budget_Expense".dim_department ( 
 	departmentcode       INT  NOT NULL  ,
 	departmentname       VARCHAR(100)    ,
 	CONSTRAINT pk_dim_department PRIMARY KEY ( departmentcode )
  );
 
-CREATE  TABLE "BudgetExpense".dim_dept_rollup ( 
+CREATE  TABLE "Budget_Expense".dim_dept_rollup ( 
 	dept_rollup          VARCHAR  NOT NULL  ,
 	dept_rollup_name     VARCHAR(100)    ,
 	CONSTRAINT pk_dim_dept_rollup PRIMARY KEY ( dept_rollup )
  );
 
-CREATE  TABLE "BudgetExpense".dim_expense ( 
+CREATE  TABLE "Budget_Expense".dim_expense ( 
 	expensecode          VARCHAR  NOT NULL  ,
 	expensename          VARCHAR(100)    ,
 	CONSTRAINT pk_dim_expensecategory PRIMARY KEY ( expensecode )
  );
 
-CREATE  TABLE "BudgetExpense".dim_fund ( 
+CREATE  TABLE "Budget_Expense".dim_fund ( 
 	fundcode             VARCHAR  NOT NULL  ,
 	fundname             VARCHAR(100)    ,
 	CONSTRAINT pk_fundcode PRIMARY KEY ( fundcode )
  );
 
-CREATE  TABLE "BudgetExpense".dim_program ( 
+CREATE  TABLE "Budget_Expense".dim_program ( 
 	programcode          VARCHAR  NOT NULL  ,
 	programname          VARCHAR(100)    ,
 	CONSTRAINT pk_dim_program PRIMARY KEY ( programcode )
  );
 
-CREATE  TABLE "BudgetExpense".dim_unit ( 
+CREATE  TABLE "Budget_Expense".dim_unit ( 
 	unitcode             VARCHAR  NOT NULL  ,
 	unitname             VARCHAR(100)    ,
 	CONSTRAINT pk_dim_unit PRIMARY KEY ( unitcode )
  );
 
-CREATE  TABLE "BudgetExpense"."facts_budget/expense" ( 
+CREATE  TABLE "Budget_Expense"."facts_budget/expense" ( 
 	fact_id              BIGINT  NOT NULL  ,
 	budget_fiscal_year   INT    ,
 	thru_quarter         INT    ,
