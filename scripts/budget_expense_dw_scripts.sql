c83615d5b47919dc6682ac995c82b3b62b11e176
FULIN2024 <FULIN.LI@BARUCHMAIL.CUNY.EDU>
2024-05-10 01:44:47 -0400

    change sql after run sql in datagrip

--- scripts/budget_expense_dw_scripts.sql
+++ scripts/budget_expense_dw_scripts.sql
@@ -1,68 +1,67 @@
-CREATE SCHEMA IF NOT EXISTS "Budget&Expense";
+CREATE SCHEMA IF NOT EXISTS "the_budget_expense";
 
-CREATE  TABLE "Budget&Expense".dim_activity (
-	ActivityCode         VARCHAR  NOT NULL  ,
-	ActivityName         VARCHAR(100)    ,
-	CONSTRAINT pk_dim_activity PRIMARY KEY ( activitycode )
+CREATE  TABLE "the_budget_expense".dim_the_activity (
+	the_activity_code         VARCHAR(100)  ,
+	the_activity_name         VARCHAR(100)    ,
+	CONSTRAINT pk_dim_the_activity PRIMARY KEY ( the_activity_code )
  );
-CREATE  TABLE "Budget&Expense".dim_department (
-	departmentcode       INT  NOT NULL  ,
-	departmentname       VARCHAR(100)    ,
-	CONSTRAINT pk_dim_department PRIMARY KEY ( departmentcode )
- );
-
-CREATE  TABLE "Budget&Expense".dim_dept_rollup (
-	dept_rollup          VARCHAR  NOT NULL  ,
-	dept_rollup_name     VARCHAR(100)    ,
-	CONSTRAINT pk_dim_dept_rollup PRIMARY KEY ( dept_rollup )
+CREATE  TABLE "the_budget_expense".dim_department (
+	the_department_code       INT  NOT NULL  ,
+	the_department_name       VARCHAR(100)    ,
+	CONSTRAINT pk_dim_department PRIMARY KEY ( the_department_code )
  );
 
-CREATE  TABLE "Budget&Expense".dim_expense (
-	expensecode          VARCHAR  NOT NULL  ,
-	expensename          VARCHAR(100)    ,
-	CONSTRAINT pk_dim_expensecategory PRIMARY KEY ( expensecode )
+CREATE  TABLE "the_budget_expense".dim_dept_rollup (
+	the_dept_rollup          VARCHAR(100)  ,
+	the_dept_rollup_name     VARCHAR(100)    ,
+	CONSTRAINT pk_dim_dept_rollup PRIMARY KEY ( the_dept_rollup )
  );
 
-CREATE  TABLE "Budget&Expense".dim_fund (
-	fundcode             VARCHAR  NOT NULL  ,
-	fundname             VARCHAR(100)    ,
-	CONSTRAINT pk_fundcode PRIMARY KEY ( fundcode )
+CREATE  TABLE "the_budget_expense".dim_expense (
+	the_expense_code          VARCHAR(100)  ,
+	the_expense_name          VARCHAR(100)    ,
+	CONSTRAINT pk_dim_expense PRIMARY KEY ( the_expense_code )
  );
 
-CREATE  TABLE "Budget&Expense".dim_program (
-	programcode          VARCHAR  NOT NULL  ,
-	programname          VARCHAR(100)    ,
-	CONSTRAINT pk_dim_program PRIMARY KEY ( programcode )
+CREATE  TABLE "the_budget_expense".dim_fund (
+	the_fund_code             VARCHAR(100)  ,
+	the_fund_name             VARCHAR(100)    ,
+	CONSTRAINT pk_dim_fund PRIMARY KEY ( the_fund_code )
  );
 
-CREATE  TABLE "Budget&Expense".dim_unit (
-	unitcode             VARCHAR  NOT NULL  ,
-	unitname             VARCHAR(100)    ,
-	CONSTRAINT pk_dim_unit PRIMARY KEY ( unitcode )
+CREATE  TABLE "the_budget_expense".dim_program (
+	the_program_code          VARCHAR(100)  ,
+	the_program_name          VARCHAR(100)    ,
+	CONSTRAINT pk_dim_program PRIMARY KEY ( the_program_code )
  );
 
-CREATE  TABLE "Budget&Expense"."facts_budget/expense" (
-	fact_id              BIGINT  NOT NULL  ,
-	budget_fiscal_year   INT    ,
-	thru_quarter         INT    ,
-	fiscal_year_id       INT    ,
-	dept_rollup          VARCHAR    ,
-	dept_rollup_name     VARCHAR(100)    ,
-	departmentcode       INT    ,
-	departmentname       VARCHAR(100)    ,
-	fundcode             VARCHAR    ,
-	fundname             VARCHAR(100)    ,
-	programcode          VARCHAR    ,
-	programname          VARCHAR(100)    ,
-	activitycode         VARCHAR    ,
-	activityname         VARCHAR(100)    ,
-	unitcode             VARCHAR  NOT NULL  ,
-	unitname             VARCHAR(100)    ,
-	expensecode          VARCHAR    ,
-	expensename          VARCHAR(100)    ,
-	budget               DECIMAL(20,2)    ,
-	expenditures         DECIMAL(20,2)    ,
-	budget_deficit       DECIMAL(20,2)    ,
-	"key"                VARCHAR    ,
-	CONSTRAINT pk_facts PRIMARY KEY ( fact_id, unitcode )
+CREATE  TABLE "the_budget_expense".dim_unit (
+	the_unit_code             VARCHAR(100)  ,
+	the_unit_name             VARCHAR(100)    ,
+	CONSTRAINT pk_dim_unit PRIMARY KEY ( the_unit_code )
+ );
+
+CREATE  TABLE "the_budget_expense".dim_facts (
+	the_fact_id              INT  ,
+	the_budget_fiscal_year   INT    ,
+	the_thru_quarter         INT    ,
+	the_dept_rollup          VARCHAR    ,
+	the_dept_rollup_name     VARCHAR(100)    ,
+	the_department_code       INT    ,
+	the_department_name       VARCHAR(100)    ,
+	the_fund_code             VARCHAR    ,
+	the_fund_name             VARCHAR(100)    ,
+	the_program_code          VARCHAR    ,
+	the_program_name          VARCHAR(100)    ,
+	the_activity_code         VARCHAR    ,
+	the_activity_name         VARCHAR(100)    ,
+	the_unit_code             VARCHAR(100)  ,
+	the_unit_name             VARCHAR(100)    ,
+	the_expense_code          VARCHAR    ,
+	the_expense_name          VARCHAR(100)    ,
+	the_budget               DECIMAL(20,2)    ,
+	the_expenditures         DECIMAL(20,2)    ,
+	the_budget_deficit       DECIMAL(20,2)    ,
+	"the_key"                VARCHAR    ,
+	CONSTRAINT pk_dim_facts PRIMARY KEY ( the_fact_id, the_unit_code )
  );
